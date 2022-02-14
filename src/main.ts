adbb57a0e14bca09cd8ba8a55be48847f319a896
Aymene <aymene01@hotmail.fr>
2022-02-14 08:59:33 +0100

    basic setup

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = parseInt(process.env.PORT) || 4242;
+  await app.listen(port);
+  console.log(`server is running on port ${await app.getUrl()}`);
+}
+bootstrap();
