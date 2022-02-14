e518952662b4d6b6b63d3f619e74f0a9bebd7dcc
Aymene <aymene01@hotmail.fr>
2022-02-14 09:48:19 +0100

    adding global prefix 'api/v1'

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT) || 4242;
+  app.setGlobalPrefix('api/v1');
   await app.listen(port);
   console.log(`server is running on port ${await app.getUrl()}`);
 }
