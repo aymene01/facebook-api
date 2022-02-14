45209aeff02ab5e1324e074b8469e1fc57d96acb
Aymene <aymene01@hotmail.fr>
2022-02-14 15:02:26 +0100

    validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT) || 4242;
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   console.log(`server is running on port ${await app.getUrl()}`);
 }
