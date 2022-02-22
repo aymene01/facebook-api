db4df1f774ce23e05f6e3b01cc5657163858bf1c
Aymene <aymene01@hotmail.fr>
2022-02-22 18:59:23 +0100

    refacto decorateur

--- src/posts/controllers/posts.controller.ts
+++ src/posts/controllers/posts.controller.ts
@@ -13,37 +13,32 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreatePostDto } from '../dtos/create-post.dto';
 import { UpdatePostDto } from '../dtos/update-post.dto';
 import { PostsService } from '../services/posts.service';
-import { Request } from 'express';
 
+@UseGuards(JwtAuthGuard)
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findPostById(@Param('id') id: string) {
     return this.postsService.getPostById(parseInt(id, 10));
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   createPost(@Req() req: any, @Body() message: CreatePostDto) {
     return this.postsService.createPost(message, req.user.id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   findAllPost() {
     return this.postsService.getAllPost();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   updatePostById(@Param('id') id: string, @Body() data: UpdatePostDto) {
     return this.postsService.updatePostById(parseInt(id, 10), data);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   deletePost(@Param('id') id: string) {
     return this.postsService.deletePostById(parseInt(id, 10));
